c16e341a978f03e4b73c5eac4a9b3ab7c80273b7
baskaraid <83570860+baskaraid@users.noreply.github.com>
2021-09-21 16:56:36 +0700

    Add files via upload

---
+++ Gabungan Nama & Kotak.py
@@ -0,0 +1,121 @@
+import pygame, sys
+from pygame import rect
+from pygame.locals import *
+import time
+
+WIDTH, HEIGHT = 400, 400
+pygame.display.set_caption('Smooth Movement')
+
+pygame.init()
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Player:
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+        self.rect = pygame.Rect(self.x, self.y, 32, 32)
+        self.color = (250, 120, 60)
+        self.velX = 0
+        self.velY = 0
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        self.speed = 4
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+    
+    def update(self):
+        self.velX = 0
+        self.velY = 0
+        if self.left_pressed and not self.right_pressed:
+            self.velX = -self.speed
+        if self.right_pressed and not self.left_pressed:
+            self.velX = self.speed
+        if self.up_pressed and not self.down_pressed:
+            self.velY = -self.speed
+        if self.down_pressed and not self.up_pressed:
+            self.velY = self.speed
+        
+        self.x += self.velX
+        self.y += self.velY
+
+        self.rect = pygame.Rect(int(self.x), int(self.y), 32, 32)
+
+
+player = Player(WIDTH/2, HEIGHT/2)
+
+font_color = (255,0,127)
+font_obj = pygame.font.Font("ElMessiri-Medium.TTF",23)
+text = "Narutama Phinda Baskara"
+img = font_obj.render(text, True, (BLACK))
+
+rect = img.get_rect()
+rect.topleft = (20,20)
+cursor = Rect(rect.topright, (3, rect.height))
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.left_pressed = True
+            if event.key == pygame.K_RIGHT:
+                player.right_pressed = True
+            if event.key == pygame.K_UP:
+                player.up_pressed = True
+            if event.key == pygame.K_DOWN:
+                player.down_pressed = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.left_pressed = False
+            if event.key == pygame.K_RIGHT:
+                player.right_pressed = False
+            if event.key == pygame.K_UP:
+                player.up_pressed = False
+            if event.key == pygame.K_DOWN:
+                player.down_pressed = False
+    
+
+
+    
+            if event.type == QUIT:
+                    running = False
+
+            if event.type == KEYDOWN:
+                if event.key == K_BACKSPACE:
+                    if len(text)>0:
+                        text = text[:-1]
+
+                else:
+                    text += event.unicode
+                    img = font_obj.render(text, True, PINK)
+                    rect.size = img.get_size()
+                    cursor.topleft = rect.topright
+
+    win.fill((GREEN))
+    pygame.draw.rect(win, (WHITE), player)
+
+    win.blit(img,rect)
+    if time.time() % 1 > 0.5:
+        pygame.draw.rect(win, GREEN, cursor)
+    pygame.display.update()
+
+    player.update()
+    pygame.display.flip()
+
+    clock.tick(120)
+    pygame.display.update()
+    
+pygame.quit()
